0721036863586a16b6abccc78de08f0767d4b348
matheuspetroni <contato@matheusgimenez.com.br>
2014-06-04 15:03:19 -0300

    atualizando padding

--- biblioteca.php
+++ biblioteca.php
@@ -8,7 +8,7 @@
 get_header();
 ?>
 	<div id="primary" class="content-area">
-		<main id="main" class="site-main" role="main">
+		<main id="main" class="site-main biblioteca-main" role="main">
 			<div class="header-area">
 				<div class="left">
 					<h1>Biblioteca</h1>
