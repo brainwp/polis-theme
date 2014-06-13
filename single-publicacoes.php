61725e80613faa3886c84420fa3d7941f9b61629
everaldomatias <matiaseveraldo@gmail.com>
2014-06-13 14:11:38 -0300

    Adicionando CSS na single de publicacao

--- single-publicacoes.php
+++ single-publicacoes.php
@@ -12,7 +12,7 @@ get_header(); ?>
 		<?php while ( have_posts() ) : the_post(); ?>
 
 		<header>
-			<h1><?php cpt_name(); ?></h1><span> • <?php top_term( 'categorias' ); ?></span><span> • <?php echo terms( 'tipos' ); ?></span>
+			<h1><?php top_term( 'categorias' ); ?></h1><span class="marcador">•</span><span><?php cpt_name(); ?></span><span class="marcador">•</span><span><?php echo terms( 'tipos' ); ?></span>
 		</header><!-- header -->
 
 		<article class="col-md-12 pull-left">
@@ -23,29 +23,33 @@ get_header(); ?>
 					<img src="<?php echo get_template_directory_uri(); ?>/img/default-publicacoes-thumb.jpg" alt="<?php the_title(); ?>" />
 				<?php } ?>
 			</div><!-- thumb -->
-			<h2><?php the_title(); ?></h2>
-			<?php the_content(); ?>
 
-			<?php if( get_field('publicacoes_autor') ): ?>
-				<span>Autor: <?php echo get_field( 'publicacoes_autor' ); ?></span>
-			<?php endif; ?>
+			<div class="content">
+				<h2><?php the_title(); ?></h2>
+				<?php the_content(); ?>
+				<div class="meta">
+					<?php if( get_field('publicacoes_autor') ): ?>
+						<span>Autor: <?php echo get_field( 'publicacoes_autor' ); ?></span><br>
+					<?php endif; ?>
 
-			<?php if( get_field('publicacoes_ano') ): ?>
-				<span>Ano: <?php echo get_field( 'publicacoes_ano' ); ?></span>
-			<?php endif; ?>
+					<?php if( get_field('publicacoes_ano') ): ?>
+						<span>Ano: <?php echo get_field( 'publicacoes_ano' ); ?></span><br>
+					<?php endif; ?>
 
-			<?php if( get_field('publicacoes_paginas') ): ?>
-				<span>Páginas: <?php echo get_field( 'publicacoes_paginas' ); ?></span>
-			<?php endif; ?>
+					<?php if( get_field('publicacoes_paginas') ): ?>
+						<span>Páginas: <?php echo get_field( 'publicacoes_paginas' ); ?></span><br>
+					<?php endif; ?>
 
-			<?php if( get_field('publicacoes_download') ): ?>
-				<?php
-					$download = get_field('publicacoes_download');
-					$file = substr( $download['url'], strrpos( $download['url'], '/' ) +1 );
-					$size = number_format( filesize( get_attached_file( $download['id'] ) ) / 1048576, 2 ) . "mb";
-				?>
-				<a href="<?php echo $download['url']; ?>" download="<?php echo $file; ?>">Download <?php echo $size; ?></a>
-			<?php endif; ?>
+					<?php if( get_field('publicacoes_download') ): ?>
+						<?php
+							$download = get_field('publicacoes_download');
+							$file = substr( $download['url'], strrpos( $download['url'], '/' ) +1 );
+							$size = number_format( filesize( get_attached_file( $download['id'] ) ) / 1048576, 2 ) . "mb";
+						?>
+						<a href="<?php echo $download['url']; ?>" download="<?php echo $file; ?>">Download <?php echo $size; ?></a>
+					<?php endif; ?>
+				</div><!-- meta -->
+			</div><!-- content -->
 
 		</article>
 
