34e6816da006c6c5224591590107ad614d5184f7
everaldomatias <matiaseveraldo@gmail.com>
2014-06-13 22:24:07 -0300

    Adiciona itens no fields.php e volta gitignore

--- fields.php
+++ fields.php
@@ -111,5 +111,135 @@
 		),
 		'menu_order' => 0,
 	));
+
+	register_field_group(array (
+		'id' => 'acf_campos-migracao',
+		'title' => 'Campos Migração',
+		'fields' => array (
+			array (
+				'key' => 'field_539b9c0acfc0f',
+				'label' => 'Data de Publicação',
+				'name' => 'mgr_data',
+				'type' => 'text',
+				'instructions' => 'Valor do campo nwd_date',
+				'conditional_logic' => array (
+					'status' => 1,
+					'rules' => array (
+						array (
+							'field' => 'null',
+							'operator' => '==',
+						),
+					),
+					'allorany' => 'all',
+				),
+				'default_value' => '',
+				'placeholder' => '',
+				'prepend' => '',
+				'append' => '',
+				'formatting' => 'html',
+				'maxlength' => '',
+			),
+			array (
+				'key' => 'field_539b8c17ddbb2',
+				'label' => 'Imagem',
+				'name' => 'mgr_imagem',
+				'type' => 'text',
+				'instructions' => 'Valor do campo nwd_image',
+				'conditional_logic' => array (
+					'status' => 1,
+					'rules' => array (
+						array (
+							'field' => 'null',
+							'operator' => '==',
+						),
+					),
+					'allorany' => 'all',
+				),
+				'default_value' => '',
+				'placeholder' => '',
+				'prepend' => '',
+				'append' => '',
+				'formatting' => 'html',
+				'maxlength' => '',
+			),
+			array (
+				'key' => 'field_539b8d6addbb3',
+				'label' => 'Fonte',
+				'name' => 'mgr_fonte',
+				'type' => 'text',
+				'instructions' => 'Valor do campo nwd_source',
+				'conditional_logic' => array (
+					'status' => 1,
+					'rules' => array (
+						array (
+							'field' => 'null',
+							'operator' => '==',
+						),
+					),
+					'allorany' => 'all',
+				),
+				'default_value' => '',
+				'placeholder' => '',
+				'prepend' => '',
+				'append' => '',
+				'formatting' => 'html',
+				'maxlength' => '',
+			),
+			array (
+				'key' => 'field_539b92e8ddbb4',
+				'label' => 'Link Externo',
+				'name' => 'mgr_link_externo',
+				'type' => 'text',
+				'instructions' => 'Valor do campo nwd_external_link',
+				'conditional_logic' => array (
+					'status' => 1,
+					'rules' => array (
+						array (
+							'field' => 'null',
+							'operator' => '==',
+						),
+					),
+					'allorany' => 'all',
+				),
+				'default_value' => '',
+				'placeholder' => '',
+				'prepend' => '',
+				'append' => '',
+				'formatting' => 'html',
+				'maxlength' => '',
+			),
+			array (
+				'key' => 'field_539b9308ddbb5',
+				'label' => 'Autor',
+				'name' => 'mgr_autor',
+				'type' => 'text',
+				'instructions' => 'Valor do campo nwd_author',
+				'default_value' => '',
+				'placeholder' => '',
+				'prepend' => '',
+				'append' => '',
+				'formatting' => 'html',
+				'maxlength' => '',
+			),
+		),
+		'location' => array (
+			array (
+				array (
+					'param' => 'post_type',
+					'operator' => '==',
+					'value' => 'noticias',
+					'order_no' => 0,
+					'group_no' => 0,
+				),
+			),
+		),
+		'options' => array (
+			'position' => 'side',
+			'layout' => 'default',
+			'hide_on_screen' => array (
+			),
+		),
+		'menu_order' => 0,
+	));
 }
 ?>
