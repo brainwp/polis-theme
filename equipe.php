cef17bced2626c96b36ead9fa2499b8e8ae13364
everaldomatias <everaldo@brasa.art.br>
2014-06-16 14:14:40 -0300

    Ajustes e pente fino na Equipe Archive Refs #38

--- equipe.php
+++ equipe.php
@@ -21,21 +21,33 @@ get_header(); ?>
 			'offset'  => $_query->offset // skip the number of users that we have per page
 		);
 
-		// The User Query
+		?>
+
+		<h1>Equipe</h1>
+
+		<?php
 		$user_query = new WP_User_Query( $args );
 		foreach ( $user_query->results as $user ) {
 			$_user   = get_userdata( $user->ID );
-			$_avatar = get_avatar( $user->ID, 200 );
+			$_avatar = get_avatar( $user->ID, 250 );
 			$_area   = get_field( 'area', 'user_' . $user->ID );
+			if ( !empty($_user->first_name)) {
+				$_name = $_user->first_name . ' ' . $_user->last_name;
+			} else {
+				$_name = $_user->user_login;
+			}
 			?>
 			<a href="<?php echo get_bloginfo('url').'/equipe/'.$_user->user_login;?>" class="col-md-3 user">
+				<div class="hover"></div>
 				<?php echo $_avatar; ?>
-				<img src="<?php bloginfo('template_url')?>/img/image-hover.png" class="hover-icon">
-				<div class="col-md-12 name reforma <?php echo $_area; ?>"><?php echo $_user->first_name .' ' . $_user->last_name;?></div>
+				<div class="col-md-12 name reforma <?php echo $_area; ?>"><?php echo $_name; ?></div>
 			</a>
 		<?php
 		}
 		?>
+
+		<?php if ( $total_pages >= 2 ) : ?>
+
 		<div class="container pagination">
 			<div class="col-md-4 col-md-offset-4">
 				<?php
@@ -61,6 +73,9 @@ get_header(); ?>
 				}
 				?>
 			</div>
-		</div>
+		</div><!-- container pagination -->
+
+		<?php endif ?>
+
 	</section>
 <?php get_footer(); ?>
