ce41f53587364cf00601faf6a44f3a8c1d48deae
everaldomatias <matiaseveraldo@gmail.com>
2014-06-17 10:29:46 -0300

    Adiciona contagem no widget Now Refs 42

--- inc/extras.php
+++ inc/extras.php
@@ -87,3 +87,38 @@ function polis_theme_setup_author() {
 	}
 }
 add_action( 'wp', 'polis_theme_setup_author' );
+
+add_action( 'dashboard_glance_items', 'polis_widget_now' );
+function polis_widget_now() {
+	$args = array(
+	'public' => true ,
+	'_builtin' => false
+	);
+ 	$output = 'object';
+ 	$operator = 'and';
+ 	$post_types = get_post_types( $args, $output, $operator );
+ 	foreach( $post_types as $post_type ) {
+  		$num_posts = wp_count_posts( $post_type->name );
+  		$num = number_format_i18n( $num_posts->publish );
+  		$text = _n( $post_type->labels->singular_name, $post_type->labels->name, intval( $num_posts->publish ) );
+  		if ( current_user_can( 'edit_posts' ) ) {
+   			echo "<li class=\"post-type-count\"><a href='edit.php?post_type=$post_type->name'>$num ";
+   			echo "$text</a></li>";
+  		} else {
+  			echo "<li class=\"post-type-count\">";
+  		}
+ 	}
+ 
+ 	$taxonomies = get_taxonomies( $args, $output, $operator );
+	foreach( $taxonomies as $taxonomy ) {
+		$num_terms = wp_count_terms( $taxonomy->name );
+		$num = number_format_i18n( $num_terms );
+		$text = _n( $taxonomy->labels->singular_name, $taxonomy->labels->name , intval( $num_terms ));
+		if ( current_user_can( 'manage_categories' ) ) {
+			echo "<li class=\"tax-count\"><a href='edit-tags.php?taxonomy=$taxonomy->name'>$num ";
+			echo "$text</a></li>";
+		} else {
+			echo "<li class=\"tax-count\">$num $text</li>";
+		}
+	}
+}
