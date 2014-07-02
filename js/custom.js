7087bd4d1f1944cc235ade1aa4e56025e35a828d
matheus <contato@matheusgimenez.com.br>
2014-07-02 10:49:49 -0300

    mapa bg

--- js/custom.js
+++ js/custom.js
@@ -1,35 +1,35 @@
 jQuery(document).ready(function () {
     var $ = jQuery.noConflict();
     $('.slider_area').carouFredSel({
-        prev      : '#prev-slider',
-        next      : '#next-slider',
+        prev: '#prev-slider',
+        next: '#next-slider',
         responsive: true,
-        width     : '100%',
-        scroll    : {
-            items       : 1,
+        width: '100%',
+        scroll: {
+            items: 1,
             pauseOnHover: true
         },
-        items     : {
-            width  : 250,
+        items: {
+            width: 250,
             visible: {
                 min: 3,
                 max: 3
             }
         }
     });
-    if($(window).width() >= 700){
+    if ($(window).width() >= 700) {
         $('.slider_documentos').carouFredSel({
-            prev      : '#prev-slider',
-            next      : '#next-slider',
+            prev: '#prev-slider',
+            next: '#next-slider',
             responsive: true,
-            width     : '100%',
-            height    : '280',
-            scroll    : {
-                items       : 1,
+            width: '100%',
+            height: '280',
+            scroll: {
+                items: 1,
                 pauseOnHover: true
             },
-            items     : {
-                width  : 250,
+            items: {
+                width: 250,
                 visible: {
                     min: 4,
                     max: 4
@@ -38,71 +38,71 @@ jQuery(document).ready(function () {
             }
         });
     }
-    $('.create_head').each(function(){
+    $('.create_head').each(function () {
         var content = $(this).html();
         var slug = $(this).attr('data-term-slug');
         var _id = $(this).attr('data-id');
-        $('.tab-pane').each(function(){
+        $('.tab-pane').each(function () {
             var id = Math.floor((Math.random() * 999999) + 1);
-            $('<header class="head-'+slug+'" data-slug="'+slug+'" id="'+id+'">'+content+'</header>').appendTo(this);
-            $('<div class="col-md-12 search-container container-'+slug+'" data-slug="'+slug+'" data-header="'+id+'"></div>').appendTo(this);
+            $('<header class="head-' + slug + '" data-slug="' + slug + '" id="' + id + '">' + content + '</header>').appendTo(this);
+            $('<div class="col-md-12 search-container container-' + slug + '" data-slug="' + slug + '" data-header="' + id + '"></div>').appendTo(this);
         });
     });
-    $('.item_search').each(function(){
+    $('.item_search').each(function () {
         var content = $(this).html();
         var area = $(this).attr('data-area-slug');
         var slug = $(this).attr('data-term-slug');
-        if (slug.length == 0){
+        if (slug.length == 0) {
             slug = 'none';
         }
         //$('#tab-'+area+' .container-'+slug+'').append('<div class="col-md-12 content>'+content+'</div>');
-        $('<div class="col-md-12 content">'+content+'</div>').appendTo('#tab-'+area+' > .search-container.container-'+slug+'');
-        console.log('#tab-'+area+' .container-'+slug+'');
+        $('<div class="col-md-12 content">' + content + '</div>').appendTo('#tab-' + area + ' > .search-container.container-' + slug + '');
+        console.log('#tab-' + area + ' .container-' + slug + '');
     });
-    $('.nav-tabs li').each(function(){
+    $('.nav-tabs li').each(function () {
         var tab = $(this).attr('data-tab-element');
-        if( $(tab).length == 0 ){
+        if ($(tab).length == 0) {
             $(this).remove();
             $(tab).remove();
         }
     });
     $('.nav-tabs li:first a').trigger('click');
-    $('.working-container').css('display','block');
-    if(location.hash.lastIndexOf('page_') != -1){
+    $('.working-container').css('display', 'block');
+    if (location.hash.lastIndexOf('page_') != -1) {
         var pageid = location.hash.slice(6);
         var siteurl = $(document.body).attr('data-siteurl');
-        var ajax = siteurl+'?ajaxPage='+pageid.trim();
+        var ajax = siteurl + '?ajaxPage=' + pageid.trim();
         $.get(ajax, function (data) {
             $('#post_ajax').html(data);
             $('.link_institucional').removeClass('atual');
-            $('#bt-'+pageid).addClass('atual');
+            $('#bt-' + pageid).addClass('atual');
         })
     }
-    $('.link_institucional').on('click', function(){
+    $('.link_institucional').on('click', function () {
         var pageid = $(this).attr('data-id');
         var siteurl = $(document.body).attr('data-siteurl');
-        var ajax = siteurl+'?ajaxPage='+pageid.trim();
+        var ajax = siteurl + '?ajaxPage=' + pageid.trim();
         $.get(ajax, function (data) {
             $('#post_ajax').html(data);
             $('.link_institucional').removeClass('atual');
-            $('#bt-'+pageid).addClass('atual');
-            location.hash = 'page_'+pageid;
+            $('#bt-' + pageid).addClass('atual');
+            location.hash = 'page_' + pageid;
             $('html, body').animate({
                 scrollTop: $('#post_ajax').offset().top
             }, 500);
         })
     });
     $('#slider2').carouFredSel({
-        prev      : '#prev-publicacao',
-        next      : '#next-publicacao',
+        prev: '#prev-publicacao',
+        next: '#next-publicacao',
         responsive: true,
-        width     : '100%',
-        scroll    : {
-            items       : 1,
+        width: '100%',
+        scroll: {
+            items: 1,
             pauseOnHover: true
         },
-        items     : {
-            width  : 250,
+        items: {
+            width: 250,
             visible: {
                 min: 4,
                 max: 4
@@ -111,51 +111,51 @@ jQuery(document).ready(function () {
     });
     var _first = $('#first').attr('data-first');
     console.log(_first)
-    $('#noticias-slider-'+_first).carouFredSel({
-        prev      : '#noticias-prev-slider-'+_first,
-        next      : '#noticias-next-slider-'+_first,
+    $('#noticias-slider-' + _first).carouFredSel({
+        prev: '#noticias-prev-slider-' + _first,
+        next: '#noticias-next-slider-' + _first,
         responsive: true,
-        width     : '100%',
-        scroll    : {
-            items       : 1,
+        width: '100%',
+        scroll: {
+            items: 1,
             pauseOnHover: true
         },
-        items     : {
-            width  : 250,
+        items: {
+            width: 250,
             visible: {
                 min: 1,
                 max: 5
             }
         }
     });
-    $('#publicacoes-slider-'+_first).carouFredSel({
-        prev      : '#publicacoes-prev-slider-'+_first,
-        next      : '#publicacoes-next-slider-'+_first,
+    $('#publicacoes-slider-' + _first).carouFredSel({
+        prev: '#publicacoes-prev-slider-' + _first,
+        next: '#publicacoes-next-slider-' + _first,
         responsive: true,
-        width     : '100%',
-        scroll    : {
-            items       : 1,
+        width: '100%',
+        scroll: {
+            items: 1,
             pauseOnHover: true
         },
-        items     : {
-            width  : 250,
+        items: {
+            width: 250,
             visible: {
                 min: 4,
                 max: 4
             }
         }
     });
-    $('#acoes-slider-'+_first).carouFredSel({
-        prev      : '#acoes-prev-slider-'+_first,
-        next      : '#acoes-next-slider-'+_first,
+    $('#acoes-slider-' + _first).carouFredSel({
+        prev: '#acoes-prev-slider-' + _first,
+        next: '#acoes-next-slider-' + _first,
         responsive: true,
-        width     : '100%',
-        scroll    : {
-            items       : 1,
+        width: '100%',
+        scroll: {
+            items: 1,
             pauseOnHover: true
         },
-        items     : {
-            width  : 250,
+        items: {
+            width: 250,
             visible: {
                 min: 4,
                 max: 4
@@ -165,97 +165,97 @@ jQuery(document).ready(function () {
     jQuery(".tabContents").hide(); // Hide all tab content divs by default
     jQuery(".tabContents:first").show(); // Show the first div of tab content by default
 
-    jQuery(".tabContaier ul li a").on('click',function () { //Fire the click event
+    jQuery(".tabContaier ul li a").on('click', function () { //Fire the click event
         var id = $(this).attr('data-id');
         var post_link = $(document.body).attr('data-siteurl') + '/?areaAjax=' + $('#main').attr('data-slug') + '&areaCatAjax=' + id + '&areaSlider=noticias';
         $.get(post_link, function (data) {
-            $('#noticias-slider-'+id).trigger('destroy');
+            $('#noticias-slider-' + id).trigger('destroy');
             $('#ajax-noticias').html(data);
-            $('#noticias-slider-'+id).html('');
-            $('#noticias-slider-'+id).carouFredSel({
-                prev      : '#noticias-prev-slider-'+id,
-                next      : '#noticias-next-slider-'+id,
+            $('#noticias-slider-' + id).html('');
+            $('#noticias-slider-' + id).carouFredSel({
+                prev: '#noticias-prev-slider-' + id,
+                next: '#noticias-next-slider-' + id,
                 responsive: true,
-                width     : '100%',
-                scroll    : {
-                    items       : 1,
+                width: '100%',
+                scroll: {
+                    items: 1,
                     pauseOnHover: true
                 },
-                items     : {
-                    width  : 250,
+                items: {
+                    width: 250,
                     visible: {
                         min: 4,
                         max: 4
                     }
                 }
             });
-            $('.ajax-item-noticias').each(function(){
+            $('.ajax-item-noticias').each(function () {
                 var content = $(this).html();
-                var item = '<li class="item">'+content+'</li>'
-                $('#noticias-slider-'+id).trigger( 'insertItem', [item, 1, false] );
+                var item = '<li class="item">' + content + '</li>'
+                $('#noticias-slider-' + id).trigger('insertItem', [item, 1, false]);
             });
         });
         var post_link = $(document.body).attr('data-siteurl') + '/?areaAjax=' + $('#main').attr('data-slug') + '&areaCatAjax=' + id + '&areaSlider=publicacoes';
         $.get(post_link, function (data) {
             $('#ajax-publicacoes').html(data);
-            $('#publicacoes-slider-'+id).trigger('destroy');
-            $('#publicacoes-slider-'+id).html('');
-            $('#publicacoes-slider-'+id).carouFredSel({
-                prev      : '#publicacoes-prev-slider-'+id,
-                next      : '#publicacoes-next-slider-'+id,
+            $('#publicacoes-slider-' + id).trigger('destroy');
+            $('#publicacoes-slider-' + id).html('');
+            $('#publicacoes-slider-' + id).carouFredSel({
+                prev: '#publicacoes-prev-slider-' + id,
+                next: '#publicacoes-next-slider-' + id,
                 responsive: true,
-                width     : '100%',
-                scroll    : {
-                    items       : 1,
+                width: '100%',
+                scroll: {
+                    items: 1,
                     pauseOnHover: true
                 },
-                items     : {
-                    width  : 250,
+                items: {
+                    width: 250,
                     visible: {
                         min: 1,
                         max: 5
                     }
                 }
             });
-            $('.ajax-item-publicacoes').each(function(){
+            $('.ajax-item-publicacoes').each(function () {
                 var content = $(this).html();
-                var item = '<li class="item">'+content+'</li>';
-                $('#publicacoes-slider-'+id).trigger( 'insertItem', [item, 1, false] );
+                var item = '<li class="item">' + content + '</li>';
+                $('#publicacoes-slider-' + id).trigger('insertItem', [item, 1, false]);
             });
         });
         var post_link = $(document.body).attr('data-siteurl') + '/?areaAjax=' + $('#main').attr('data-slug') + '&areaCatAjax=' + id + '&areaSlider=acoes';
         $.get(post_link, function (data) {
-            $('#acoes-slider-'+id).trigger('destroy');
-            $('#acoes-slider-'+id).html('');
+            $('#acoes-slider-' + id).trigger('destroy');
+            $('#acoes-slider-' + id).html('');
             $('#ajax-acoes').html(data);
-            $('#acoes-slider-'+id).carouFredSel({
-                prev      : '#acoes-prev-slider-'+id,
-                next      : '#acoes-next-slider-'+id,
+            $('#acoes-slider-' + id).carouFredSel({
+                prev: '#acoes-prev-slider-' + id,
+                next: '#acoes-next-slider-' + id,
                 responsive: true,
-                width     : '100%',
-                scroll    : {
-                    items       : 1,
+                width: '100%',
+                scroll: {
+                    items: 1,
                     pauseOnHover: true
                 },
-                items     : {
-                    width  : 250,
+                items: {
+                    width: 250,
                     visible: {
                         min: 1,
                         max: 4
                     }
                 }
             });
-            $('.ajax-item-acoes').each(function(){
+            $('.ajax-item-acoes').each(function () {
                 var content = $(this).html();
-                var item = '<li class="item">'+content+'</li>';
-                $('#acoes-slider-'+id).trigger( 'insertItem', [item, 1, false] );
+                var item = '<li class="item">' + content + '</li>';
+                $('#acoes-slider-' + id).trigger('insertItem', [item, 1, false]);
             });
         });
         var activeTab = jQuery(this).attr("href"); // Catch the click link
         jQuery(".tabContaier ul li a").removeClass("active"); // Remove pre-highlighted link
         jQuery(this).addClass("active"); // set clicked link to highlight state
         jQuery(".tabContents").hide(); // hide currently visible tab content div
-        $('#tab'+id).show();
+        $('#tab' + id).show();
         jQuery(activeTab).fadeIn(); // show the target tab content div by matching clicked link.
         $('#ajax-acoes').html("");
         $('#ajax-publicacoes').html("");
@@ -282,16 +282,16 @@ jQuery(document).ready(function () {
             $('#hide-ajax').html(data);
             $('#slider2').trigger("destroy");
             $('#slider2').carouFredSel({
-                prev      : '#prev-slider',
-                next      : '#next-slider',
+                prev: '#prev-slider',
+                next: '#next-slider',
                 responsive: true,
-                width     : '100%',
-                scroll    : {
-                    items       : 1,
+                width: '100%',
+                scroll: {
+                    items: 1,
                     pauseOnHover: true
                 },
-                items     : {
-                    width  : 250,
+                items: {
+                    width: 250,
                     visible: {
                         min: 4,
                         max: 4
@@ -301,14 +301,14 @@ jQuery(document).ready(function () {
         });
         return false;
     });
-    $('#busca-biblioteca-bt').on('click', function(){
+    $('#busca-biblioteca-bt').on('click', function () {
         $('#busca-biblioteca').submit();
     });
-    $('#busca-biblioteca').on('submit', function(e){
-        if( $('#area-input').val().trim() == '' ){
+    $('#busca-biblioteca').on('submit', function (e) {
+        if ($('#area-input').val().trim() == '') {
             e.preventDefault();
             var siteurl = $(document.body).attr('data-siteurl');
-            var ajax_req = siteurl + '?isBibliotecaCountAjax=true&key='+$('#key').val()+'&tipo='+$('#select_tipo').val()+'&categoria='+$('#select_cat').val()+'&anomin='+$('#select_anomin').val()+'&anomax='+$('#select_anomax').val();
+            var ajax_req = siteurl + '?isBibliotecaCountAjax=true&key=' + $('#key').val() + '&tipo=' + $('#select_tipo').val() + '&categoria=' + $('#select_cat').val() + '&anomin=' + $('#select_anomin').val() + '&anomax=' + $('#select_anomax').val();
             $('#ajax-counter').load(ajax_req);
             $('html, body').animate({
                 scrollTop: $('#biblioteca-require-position').offset().top
@@ -316,20 +316,53 @@ jQuery(document).ready(function () {
             $('#biblioteca-require').fadeIn('slow');
         }
     });
-    $(document).on("click", ".envia-area", function(){
+    $(document).on("click", ".envia-area", function () {
         var area = $(this).attr('data-area');
         $('#area-input').val(area);
         $('#busca-biblioteca').submit();
     });
-    $(document).on("click", ".bt-categorias", function(){
+    $(document).on("click", ".bt-categorias", function () {
         var cat = $(this).attr('data-categoria');
         $('#categoria-hidden').val(cat);
         $('#busca-biblioteca-mini').submit();
     });
 
-    $('#busca-biblioteca-mini-bt').on('click', function(){
+    $('#busca-biblioteca-mini-bt').on('click', function () {
         $('#busca-biblioteca-mini').submit();
     });
-    var _ajax = '?isAjaxSubCount=true&key='+$('#key').val()+'&categoria='+$('#categoria-hidden').val()+'&area='+$('#area-hidden').val();
+    var _ajax = '?isAjaxSubCount=true&key=' + $('#key').val() + '&categoria=' + $('#categoria-hidden').val() + '&area=' + $('#area-hidden').val();
     $('#ajax-biblioteca-sub-count').load($(document.body).attr('data-siteurl') + '/' + _ajax);
+    if ($(window).width() >= 700) {
+        $(document.body).css('background', 'transparent');
+        $('#map-bg').contents().find('#mp-embed-bar').remove();
+        $('#map-bg').css('height', $(window).height() + 'px');
+        $('#map-bg').css('width', $(window).width() + 'px');
+        $('#map-bg').css('display', 'block');
+        function initialize() {
+            var mapOptions = {
+                center: new google.maps.LatLng(-23.544891, -46.644645),
+                zoom: 20,
+                mapTypeId: google.maps.MapTypeId.ROADMAP,
+                disableDefaultUI: true,
+                scrollwheel: false,
+                navigationControl: false,
+                mapTypeControl: false,
+            }
+            var styles = {
+                elementType: "labels",
+                stylers: [
+                    { visibility: "off" }
+                ],
+                featureType: "poi",
+                stylers: [
+                    { visibility: "off" }
+                ]
+            }
+            var map = new google.maps.Map(document.getElementById('map-bg'),
+                mapOptions);
+            map.setOptions({styles: styles});
+        }
+
+        google.maps.event.addDomListener(window, 'load', initialize);
+    }
 });
